1ebb650b6282497fe4302cc972121155abaa2e20
MuskanSingh-1 <130390291+MuskanSingh-1@users.noreply.github.com>
2026-04-04 23:07:14 +0530

    Update api.py

--- api.py
+++ api.py
@@ -381,9 +381,9 @@ def predict_flood(state: str, district: str, req: FloodRequest):
         X = np.array(features).reshape(1, -1)
         prob = model.predict_proba(X)[0][1]
 
-        if prob < 0.65:
+        if prob > 0.65:
             risk = "Low"
-        elif prob < 0.90:
+        elif prob > 0.90:
             risk = "Moderate"
         else:
             risk = "High"
